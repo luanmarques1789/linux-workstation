7695029fac0fecde1709bb20c7c235a7f3b2cb7a
Luan Marques <luanmarques1789@gmail.com>
2022-04-11 18:37:53 -0300

    executable script
    Functions added (encapsulated code) and starting to apply conditionals.
    
    PS.: more tests are required. Not 100% run perfectly.

--- ubuntu-workstation.sh
+++ ubuntu-workstation.sh
@@ -1,16 +1,81 @@
 #!/bin/bash
 
-## Removing evently apt blocks ##
-sudo rm /var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock ;
+## DEFINING FUNCTIONS ##
 
-## Updating the repository ##
-sudo apt update &&
+update() {
+  sudo apt update -y
+}
 
-## Install flatpak and add flathub's repository ##
-sudo apt install flatpak &&
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &&
-sudo apt install gnome-software-plugin-flatpak && # flatpak's GUI
+upgrade() {
+  sudo apt upgrade -y
+}
 
-# Install flatpak's apps
-flatpak install -y flathub com.discordapp.Discord --user && 
-flatpak install -y flathub io.dbeaver.DBeaverCommunity --user &&
+clear_setup() {
+  update &&
+  sudo apt dist-upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y
+}
+
+remove_locks() {
+  path1="/var/lib/dpkg/lock-frontend"
+  path2="/var/cache/apt/archives/lock"
+
+  if [[ -d $path1 ]]; then
+    sudo rm $path1;
+  fi
+
+  if [[ -d $path2 ]]; then
+    sudo rm $path2;
+  fi
+}
+
+
+add_architectures() {
+  ## architecture 32 bits ##
+  sudo dpkg --add-architecture i386;
+}
+
+apt_add() {
+  sudo apt install -y apt-transport-https curl git-all
+}
+
+pre_install_flatpak() {
+  ## Install flatpak and add flathub's repository ##
+  sudo apt install -y flatpak &&
+  flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &&
+  sudo apt install -y gnome-software-plugin-flatpak # flatpak's GUI
+}
+
+flatpak_add() {
+  app=$1
+  sudo flatpak install -y $app --user;
+}
+
+install_flatpak_apps(){
+  pre_install_flatpak &&
+  flatpak_add com.discordapp.Discord
+  flatpak_add io.dbeaver.DBeaverCommunity
+  flatpak_add org.mozilla.Thunderbird
+}
+
+add_brave() {
+  update &&
+  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg \ 
+    https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg &&
+
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] \
+    https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list &&
+
+  update && upgrade &&
+  sudo apt install -y brave-browser;
+}
+
+## RUN FUNCTIONS ##
+
+remove_locks
+add_architectures
+clear_setup &&
+apt_add &&
+add_brave
+install_flatpak_apps
+clear_setup
+echo "Script finished!"
