e155714ed7023d81495219a736eb1d395cdcf798
Luan Marques <luanmarques1789@gmail.com>
2022-05-15 11:29:10 -0300

    mysql-server installation

--- scripts/other_programs.sh
+++ scripts/other_programs.sh
@@ -26,5 +26,36 @@ function install_nodejs() {
   fi
 }
 
+function install_mysql() {
+  package="mysql-server"
+
+  if ! dpkg -l | grep -q "$package"; then
+    printf "${BLUE}[TASK]${NO_COLOR} - Installing ${ORANGE}$package${NO_COLOR}...\n"
+
+    source /etc/os-release
+
+    # specific installation for Linux distro XYZ
+    if [[ $ID == 'debian' ]]; then
+      download="mysql-apt-config_0.8.22-1_all.deb"
+
+      # downloading APT MySQL repo for after installing mysql-server
+      test ! -e programs/${download} && wget -q https://repo.mysql.com/${download} -P programs/
+
+      # installing APT MySQL repo
+      sudo dpkg -i programs/${download} &&
+        update
+
+      pass="YES"
+    fi
+
+    [[ $pass == "YES" ]] && printf "${BLUE}[TASK]${NO_COLOR} - Still installing...\n"
+    sudo apt install -y $package
+    validate_pkg $package
+  else
+    printf "${PURPLE}[INFO]${NO_COLOR} - package ${ORANGE}$package${NO_COLOR} is already installed!\n"
+  fi
+}
+
 remove_locks && update && upgrade
 install_nodejs
+install_mysql
