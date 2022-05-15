e14ab5e047d4a3c3266f858bf72c61ef85efcc0c
Luan Marques <luanmarques1789@gmail.com>
2022-05-15 11:33:20 -0300

    update script improved

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,23 +6,27 @@ function remove_locks() {
 
   if [[ -d $path1 ]]; then
     sudo rm $path1 &&
-      echo "lock-frontend was removed successfully"
+      printf "lock-frontend was removed successfully\n"
   fi
 
   if [[ -d $path2 ]]; then
     sudo rm $path2 &&
-      echo "lock was removed successfully"
+      printf "lock was removed successfully\n"
   fi
 }
 
 function update() {
-  printf "${BLUE}[TASK]${NO_COLOR} - Updating...\n" &&
-    sudo apt update -y &>/dev/null
+  printf "${BLUE}[TASK]${NO_COLOR} - Updating..." &&
+    sudo apt update -y &>/dev/null &&
+    printf " >>> ${GREEN}Updated!${NO_COLOR}\n" ||
+    printf " >>> ${RED}Failed!${NO_COLOR}\n"
 }
 
 function upgrade() {
-  printf "${BLUE}[TASK]${NO_COLOR} - Upgrading...\n" &&
-    sudo apt upgrade -y &>/dev/null
+  printf "${BLUE}[TASK]${NO_COLOR} - Upgrading..." &&
+    sudo apt upgrade -y &>/dev/null &&
+    printf " >>> ${GREEN}Upgraded!${NO_COLOR}\n" ||
+    printf " >>> ${RED}Failed!${NO_COLOR}\n"
 }
 
 function clear_setup() {
